4e0b23cbc6b0e4ecaebd58412098c40c5de90a1e
Baptiste GUILLON <baptiste.guillon2021@campus-eni.fr>
2022-01-12 10:21:09 +0100

    DAO Utilisateur

--- src/main/java/fr/eni/enchere/dal/UtilistaeurDAO.java
+++ src/main/java/fr/eni/enchere/dal/UtilistaeurDAO.java
@@ -1,5 +1,17 @@
 package fr.eni.enchere.dal;
 
-public interface UtilistaeurDAO {
+import java.util.List;
 
+import fr.eni.enchere.bo.Article;
+import fr.eni.enchere.bo.Utilisateur;
+
+public interface UtilistaeurDAO {
+	
+	void insert (Utilisateur nouvelUtilisateur) throws DALException;
+	void update (Utilisateur utilisateurAModifier) throws DALException;
+	void delete (Integer noUtilisateur) throws DALException;
+	
+	List<Article> selectAll() throws DALException;
+	
+	Utilisateur selectById (Integer noUtilisateur) throws DALException;
 }
